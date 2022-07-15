29128ecdef14310abea67e8ab37a00dab832ee23
Daniel Zenzes <daniel@zenzes.me>
2022-07-15 15:32:58 +0200

    counters can be deleted, some stzling and refactoring

--- src/components/EmButton.vue
+++ src/components/EmButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="px-4 py-2 font-bold text-white bg-blue-400 rounded hover:bg-blue-500"
+    class="self-center px-4 py-2 font-bold text-white bg-blue-400 rounded hover:bg-blue-500"
     @click="$emit('click')"
   >
     <slot></slot>
